fdd56f76bec3a36e3b13944d3baa1c6251898ab5
Hasan Basri OY <hasanbasrioy@gmail.com>
2024-04-24 11:38:06 +0300

    Reformat update_engine

--- device.mk
+++ device.mk
@@ -39,10 +39,12 @@ PRODUCT_PACKAGES_DEBUG += \
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
-    cppreopts.sh \
+    checkpoint_gc
+
+PRODUCT_PACKAGES += \
     update_engine \
-    update_verifier \
-    update_engine_sideload
+    update_engine_sideload \
+    update_verifier
 
 # Vibrator modules
 TARGET_RECOVERY_DEVICE_MODULES += \
